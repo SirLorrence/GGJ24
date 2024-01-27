ee9e94be8f1fec5773ad18e83503fee9b0b08bbe
Laurence <sirlaurence@protonmail.com>
2024-01-27 13:02:54 -0800

    Item WIP

---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace DefaultNamespace {
+  [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+  public class ItemData : ScriptableObject {
+    
+  }
+}
