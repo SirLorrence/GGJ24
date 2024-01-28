a4f409242f8d1ddd6e990b216193d38f78acfeed
Laurence <sirlaurence@protonmail.com>
2024-01-28 08:44:18 -0800

    Level Restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
   public float RemainingTime => m_remainingTime;
@@ -7,21 +9,27 @@ public class GameManager : MonoBehaviour {
 
   [SerializeField] private float m_employmentWeight;
   [SerializeField] private BossBehaviour m_bossGuy;
+  [SerializeField] private float m_sceneRestartTime = 5f;
 
+  [Header("Debug Settings")] [SerializeField]
+  private bool m_enableDebug;
+
+  [Tooltip("When Debug option is set, this will be the game time used")] [SerializeField]
+  private float m_timeDebug = 180;
+
+  [SerializeField] private GameState m_currentGameState;
 
   private enum GameState {
-    kMenu,
     kRunning,
     kLoose,
     kWin,
   }
 
-  [SerializeField] private GameState m_currentGameState = GameState.kMenu;
 
   private const float m_maxTime = 180; // 3min 
   private float m_remainingTime;
 
-  private bool m_initGame;
+  private bool m_initLoadGame;
 
   private static GameManager m_instance;
 
@@ -52,15 +60,11 @@ public class GameManager : MonoBehaviour {
 
   private void Start() {
     m_employmentWeight = 0;
-
-
     OnGameStart();
   }
 
   private void Update() {
     switch (m_currentGameState) {
-      case GameState.kMenu:
-        break;
       case GameState.kRunning:
         if (!CheckEndGameState()) {
           m_remainingTime -= Time.deltaTime;
@@ -68,8 +72,18 @@ public class GameManager : MonoBehaviour {
 
         break;
       case GameState.kLoose:
+        //TODO: loose screen
+        if (m_initLoadGame) {
+          StartCoroutine(OnGameEnd());
+        }
+
         break;
       case GameState.kWin:
+        //TODO: win screen
+        if (m_initLoadGame) {
+          StartCoroutine(OnGameEnd());
+        }
+
         break;
       default:
         throw new ArgumentOutOfRangeException();
@@ -81,19 +95,21 @@ public class GameManager : MonoBehaviour {
   bool CheckEndGameState() {
     if (RemainingTime <= 0) {
       m_currentGameState = GameState.kLoose;
+      m_initLoadGame = true;
       return true;
     }
 
     if (m_employmentWeight > 1) {
       m_currentGameState = GameState.kWin;
+      m_initLoadGame = true;
       return true;
     }
 
     if (m_employmentWeight < -1) {
       m_currentGameState = GameState.kLoose;
+      m_initLoadGame = true;
       return true;
     }
-
 
     return false;
   }
@@ -103,13 +119,23 @@ public class GameManager : MonoBehaviour {
 
   #region GameActions
 
-  public void OnGameStart() {
+  private void OnGameStart() {
     m_remainingTime = m_maxTime;
+    m_employmentWeight = 0;
+#if DEBUG && UNITY_EDITOR
+    if (m_enableDebug) {
+      m_remainingTime = m_timeDebug;
+    }
+#endif
     m_currentGameState = GameState.kRunning;
   }
 
-  public void OnGameEnd() {
-    m_currentGameState = GameState.kLoose;
+  private IEnumerator OnGameEnd() {
+    m_initLoadGame = false;
+    var levelID = SceneManager.GetActiveScene().buildIndex;
+    yield return new WaitForSeconds(m_sceneRestartTime);
+    SceneManager.LoadScene(levelID);
+    OnGameStart();
   }
 
   #endregion
